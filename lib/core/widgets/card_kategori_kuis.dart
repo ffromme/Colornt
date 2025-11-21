3453fbcf51f35fcb32cb0cf74c73dce03c9b1c26
Rizki Ilham Putra Pratama <yamsecondary@gmail.com>
2025-11-21 21:09:17 +0700

    feat: Menu Kuis

---
+++ lib/core/widgets/card_kategori_kuis.dart
@@ -0,0 +1,56 @@
+import 'package:appbutawarna/core/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class CardKategoriKuis extends StatelessWidget {
+  final String title;
+  final String description;
+  final String image;
+
+  const CardKategoriKuis({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            padding: EdgeInsets.all(16),
+            width: 300,
+            height: 400,
+            decoration: BoxDecoration(
+              color: AppTheme.primaryColor,
+              borderRadius: BorderRadius.circular(28),
+            ),
+            child: Image.asset(
+                image,
+            ),
+          ),
+        ),
+        const SizedBox(height: 12,),
+        Text(
+          title,
+          style: TextStyle(
+            color: AppTheme.textPrimary,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        const SizedBox(height: 8,),
+        Text(
+          description,
+          style: TextStyle(
+            color: AppTheme.textSecondary,
+            fontSize: 16,
+          ),
+        )
+      ],
+    );
+  }
+}
